12dc3f8e8340dabe14b934c9e6c28c641c5eb168
dsmit304 <dsmit304@jhu.edu>
2020-10-11 16:12:44 -0400

    Added comments. Fixed code to handle spacing issues since ng-model only trims ends of string and not between commas which is needed for split()

--- assignment6/js/app.js
+++ assignment6/js/app.js
@@ -5,6 +5,7 @@
     .controller('LunchCheckController', LunchCheckController);
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
+        // Variable Declaration
         $scope.changeBorder = "";
         $scope.changeColor = "";
         $scope.comment = "";
@@ -12,44 +13,39 @@
         $scope.message = "";
 
         $scope.checkIntakeAmount = function () {
-            var border = "";
-            var color = "";
-            var comment = "";
+            // Variable Declaration
+            $scope.comment = ""; // Clears
             var count = 0;
             var lunch = $scope.lunch;
-            var menu = lunch.split(", ");
-            var message = "";
+            var menu = lunch.split(",");
 
-            if(!lunch) {
-                border = "no-data-border";
-                color = "no-data-color";
-                message = "Please enter data first";
-            } else {
-                for(var i = 0; i < menu.length; i++) {
-                    if(!menu[i]) {
-                        comment = "Empty items are NOT counted";
-                    } else {
+            if(!lunch) { // Checks for entered data
+                $scope.changeBorder = "no-data-border";
+                $scope.changeColor = "no-data-color";
+                $scope.message = "Please enter data first";
+            } else { // Entered data found
+                for(var i = 0; i < menu.length; i++) { // Loops through array of lunch items
+                    menu[i] = menu[i].replace(/  +/g, ' '); // Converts mutliple spaces into singlar space
+                    if(!menu[i] || menu[i] == " ") { // Checks if current index is empty or a single space
+                        $scope.comment = "Empty items are NOT counted";
+                    } else { // Adds to count if current index has data
                         count++;
                     }
                 }
-                if(count <= 1 && menu[0] == "") {
-                    border = "no-data-border";
-                    color = "no-data-color";
-                    message = "Please enter data first";
-                } else if(count <= 3 ) {
-                    border = "data-border";
-                    color = "data-color";
-                    message = "Enjoy!";
-                } else {
-                    border = "data-border";
-                    color = "data-color";
-                    message = "Too much!";
+                if(count <= 1 && menu[0] == "") { // Checks if count is 1 or below and the first menu item is empty
+                    $scope.changeBorder = "no-data-border";
+                    $scope.changeColor = "no-data-color";
+                    $scope.message = "Please enter data first";
+                } else if(count <= 3 ) { // Checks if count is 3 or below
+                    $scope.changeBorder = "data-border";
+                    $scope.changeColor = "data-color";
+                    $scope.message = "Enjoy!";
+                } else { // Count is greater than 3
+                    $scope.changeBorder = "data-border";
+                    $scope.changeColor = "data-color";
+                    $scope.message = "Too much!";
                 }
             }
-            $scope.changeBorder = border;
-            $scope.changeColor = color;
-            $scope.comment = comment;
-            $scope.message = message;
         }
     }
 })();
