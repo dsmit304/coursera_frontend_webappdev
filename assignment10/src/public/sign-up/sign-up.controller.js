f4c3aa04713b3a3e0f7f8bf4a7b9482bb5ff505f
Danni <dsmit304@dannis-mbp.myfiosgateway.com>
2020-12-10 21:53:40 -0500

    Finished task 3?

--- assignment10/src/public/sign-up/sign-up.controller.js
+++ assignment10/src/public/sign-up/sign-up.controller.js
@@ -8,20 +8,25 @@
     function SignUpController(MenuService, StoreDataService) {
       var sign = this;
 
-      sign.submit = function () {
+      sign.checkFavItem = function() {
         var promise = MenuService.getMenuShortNames(sign.user.favItem);
 
         promise.then(function (results) {
-            if (results.error) {
-                sign.favItemNotFound = true;
-            } else {
-                sign.favItemNotFound = false;
-                sign.user.name = results.name;
-                sign.user.description = results.description;
-                StoreDataService.setUser(sign.user);
-                sign.dataSaved = true;
-            }
+          if (results.error) {
+              sign.favItemNotFound = true;
+          } else {
+              sign.favItemNotFound = false;
+              sign.user.name = results.name;
+              sign.user.description = results.description;
+          }
         });
+      }
+  
+      sign.submit = function () {
+        if (!sign.favItemNotFound) {
+            StoreDataService.setUser(sign.user);
+            sign.dataSaved = true;
+        }
       }
     }
 })();
