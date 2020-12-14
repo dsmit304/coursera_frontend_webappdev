7b796dbd6d71446b5e2b4997a6fec8081a64bed1
Danni <dsmit304@Dannis-MacBook-Pro.local>
2020-12-13 22:40:49 -0500

    Cleanup. Still having issues with controller method being undefined and public module not being found.

--- assignment10/spec/sign-up.controller.spec.js
+++ assignment10/spec/sign-up.controller.spec.js
@@ -7,12 +7,12 @@ describe('Unit Test for Favorite Item Method:', function () {
                 var service = this;
 
                 service.getMenuShortName = function (shortName) {
-                    //$httpBackend.whenGET(ApiBasePath + '/menu_items/CU21.json').respond([{"id":91,"short_name":"CU21","name":"Curry Chicken","description":"white meat chicken sauteed with ionions, peas, carrots, and broccoli","price_small":11.95,"price_large":14.95,"small_portion_name":"pint","large_portion_name":"large","created_at":"2020-11-19T00:35:47.270Z","updated_at":"2020-11-19T00:35:47.270Z","category_short_name":"CU","image_present":true}]);
-                    //menu.getMenuShortName(shortName).then(function(response) {
-                    //    expect(response.data).toEqual([{"id":91,"short_name":"CU21","name":"Curry Chicken","description":"white meat chicken sauteed with ionions, peas, carrots, and broccoli","price_small":11.95,"price_large":14.95,"small_portion_name":"pint","large_portion_name":"large","created_at":"2020-11-19T00:35:47.270Z","updated_at":"2020-11-19T00:35:47.270Z","category_short_name":"CU","image_present":true}]);
-                    //});
-                    //$httpBackend.flush();
-                    return '{"id":91,"short_name":"CU21","name":"Curry Chicken","description":"white meat chicken sauteed with ionions, peas, carrots, and broccoli","price_small":11.95,"price_large":14.95,"small_portion_name":"pint","large_portion_name":"large","created_at":"2020-11-19T00:35:47.270Z","updated_at":"2020-11-19T00:35:47.270Z","category_short_name":"CU","image_present":true}]);';
+                    var dummyData = {data:{"id":91,"short_name":"CU21","name":"Curry Chicken","description":"white meat chicken sauteed with ionions, peas, carrots, and broccoli","price_small":11.95,"price_large":14.95,"small_portion_name":"pint","large_portion_name":"large","created_at":"2020-11-19T00:35:47.270Z","updated_at":"2020-11-19T00:35:47.270Z","category_short_name":"CU","image_present":true}};
+                    $httpBackend.whenGET(ApiBasePath + '/menu_items/CU21.json').respond([200, dummyData]);
+                    menu.getMenuShortName(shortName).then(function(response) {
+                        expect(response.data).toEqual([200, dummyData]);
+                    });
+                    $httpBackend.flush();
                 };
             });
             $provide.service('StoreDataServiceMock', function() {
