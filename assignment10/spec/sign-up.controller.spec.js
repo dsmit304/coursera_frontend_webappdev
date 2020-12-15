40b0d0ecd10b0e901e790bc820756263a9c58a6c
Danni <dsmit304@Dannis-MacBook-Pro.local>
2020-12-14 21:16:02 -0500

    Task 4: Unit Test is still erroring but I can't find the issue causing it. I've tried for hours to fix this. At this point I'm pretty much giving up on it.

--- assignment10/spec/sign-up.controller.spec.js
+++ assignment10/spec/sign-up.controller.spec.js
@@ -1,20 +1,24 @@
-describe('Unit Test for Favorite Item Method:', function () {
+describe('SignUpController.checkFavItem()', function () {
+    beforeEach(module('public'));
     var $httpBackend, ApiBasePath, $controller, signUpController;
 
+    beforeEach(inject(function (_$controller_, _$httpBeckend_, MenuServiceMock, StoreDataServiceMock) {
+        $controller = _$controller_;
+        $httpBackend = _$httpBeckend_;
+
+        signUpController = $controller('SignUpController', {'MenuService': MenuServiceMock, 'StoreDataService': StoreDataServiceMock});
+    }));
     beforeEach(function () {
         module(function ($provide) {
             $provide.service('MenuServiceMock', function() {
                 var service = this;
 
                 service.getMenuShortName = function (shortName) {
-                    var dummyData = {data:{"id":91,"short_name":"CU21","name":"Curry Chicken","description":"white meat chicken sauteed with ionions, peas, carrots, and broccoli","price_small":11.95,"price_large":14.95,"small_portion_name":"pint","large_portion_name":"large","created_at":"2020-11-19T00:35:47.270Z","updated_at":"2020-11-19T00:35:47.270Z","category_short_name":"CU","image_present":true}};
-                    $httpBackend.whenGET(ApiBasePath + '/menu_items/CU21.json').respond([200, dummyData]);
-                    menu.getMenuShortName(shortName).then(function(response) {
-                        expect(response.data).toEqual([200, dummyData]);
-                    });
+                    var dummyData = {data: {"id":91,"short_name":"CU21","name":"Curry Chicken","description":"white meat chicken sauteed with ionions, peas, carrots, and broccoli","price_small":11.95,"price_large":14.95,"small_portion_name":"pint","large_portion_name":"large","created_at":"2020-11-19T00:35:47.270Z","updated_at":"2020-11-19T00:35:47.270Z","category_short_name":"CU","image_present":true}};
+                    $httpBackend.whenGET(ApiBasePath + '/menu_items/CU21.json').respond([dummyData]);
                     $httpBackend.flush();
                 };
-            });
+            }),
             $provide.service('StoreDataServiceMock', function() {
                 var service = this;
 
@@ -23,17 +27,14 @@ describe('Unit Test for Favorite Item Method:', function () {
                 };
             });
         });
-        module('public');
     });
 
-    beforeEach(inject(function (_$controller_, MenuServiceMock, StoreDataServiceMock) {
-        $controller = _$controller_;
+    it('should return json blob of favorite item', function() {
+        var user = {"firstname":"Jane","lastname":"Doe","email":"janedoe@yahoo.com","phone":"123-456-7890","favItem":"CU21"};
+        var promise = signUpController.checkFavItem(user.favItem);
 
-        signUpController = $controller('SignUpController', {MenuService: MenuServiceMock, StoreDataService: StoreDataServiceMock});
-    }));
-
-    it('should return favorite item details', function() {
-        signUpController.checkFavItem();
-        expect(signUpController.favItemNotFound).toBe(false);
+        promise.then(function() {
+            expect(signUpController.favItemNotFound).toBeFalsy();
+        })
     });
 });
